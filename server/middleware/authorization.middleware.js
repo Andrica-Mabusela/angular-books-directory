757ab715e7ecf4c1faa780efe72c1d3e5e37ce07
Tshepho Mabusela <tshepho.mabusela@dadevs.co.za>
2022-05-06 16:17:18 +0200

    changed data stored in jwt auth token

--- server/middleware/authorization.middleware.js
+++ server/middleware/authorization.middleware.js
@@ -5,6 +5,7 @@ module.exports.isAuthorized = (req, res, next) => {
     const authToken = req.headers['authorization'];
 
     jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2m' }, (err, decoded) => {
-        console.log(err.message)
+        if(err) console.log(err.message)
+        console.log(decoded)
     })
 }
